fe4a100c6ca48abc1c1adbcc97d4f9f4baac7897
niksonstha <sthanikson10@gmail.com>
2024-02-02 12:39:48 +0545

    Update Vite config to use process.env directly instead of stringifying it.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   define: {
-    "process.env.VITE_TOKEN": JSON.stringify(process.env.VITE_TOKEN),
+    "process.env": process.env,
   },
 });
